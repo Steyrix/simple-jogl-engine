4b5cd1f835f9980860aa7abab0af66fc6bb96e26
Georgy Pevnev <steyrixio@gmail.com>
2019-04-06 16:50:37 +0600

    equals method implemented

--- src/main/java/engine/core/util/utilgeometry/PointF.java
+++ src/main/java/engine/core/util/utilgeometry/PointF.java
@@ -5,17 +5,27 @@ public class PointF {
     public final float x;
     public final float y;
 
-    public PointF(){
+    public PointF() {
         this.x = 0f;
         this.y = 0f;
     }
 
-    public PointF(final float x, final float y){
+    public PointF(final float x, final float y) {
         this.x = x;
         this.y = y;
     }
 
-    public String toString(){
+    public String toString() {
         return "(x: " + x + " ; " + "y: " + y + " )";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (o instanceof PointF) {
+            return this.x == ((PointF) o).x && this.y == ((PointF) o).y;
+        }
+
+        return false;
     }
 }
