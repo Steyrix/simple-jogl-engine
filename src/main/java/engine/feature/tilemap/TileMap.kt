411d61b480bc9dc543842a3defc5f19975f40335
Georgy Pevnev <steyrixio@gmail.com>
2019-04-06 18:24:53 +0600

    implement tile and tiled map classes

---
+++ src/main/java/engine/feature/tilemap/TileMap.kt
@@ -0,0 +1,32 @@
+package engine.feature.tilemap
+
+import com.jogamp.opengl.GL4
+import engine.feature.primitives.Rectangle
+import engine.feature.primitives.Rectangle.RECTANGLE_BUFFER
+import engine.feature.shader.Shader
+
+class Tile (private val width: Float,
+            private val height: Float,
+            private val texturePath: String,
+            private val posX: Float,
+            private val posY: Float) {
+
+    private fun toOpenGlObject(gl: GL4): Rectangle {
+
+        val out = Rectangle(gl, width, height, 0)
+        out.initRenderData(arrayOf(texturePath), false, RECTANGLE_BUFFER)
+
+        return out
+    }
+
+    fun draw(gl: GL4, shader: Shader) {
+        toOpenGlObject(gl).draw(posX, posY, width, height, 0f, shader)
+    }
+}
+
+class TileMap(private val tiles: ArrayList<Tile>) {
+
+    fun draw(gl: GL4, shader: Shader) {
+        tiles.forEach { it.draw(gl, shader) }
+    }
+}
