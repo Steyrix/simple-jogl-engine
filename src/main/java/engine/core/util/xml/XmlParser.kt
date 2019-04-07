1c5dca49497de712f759dcc3cde79be63fedd4ea
Georgy Pevnev <steyrixio@gmail.com>
2019-04-07 07:36:29 +0600

    move logic of creating xml document to xml util

--- src/main/java/engine/core/util/xml/XmlParser.kt
+++ src/main/java/engine/core/util/xml/XmlParser.kt
@@ -1,4 +1,36 @@
 package engine.core.util.xml
 
+import org.w3c.dom.Document
+import java.io.File
+import java.lang.IllegalArgumentException
+import javax.xml.parsers.DocumentBuilderFactory
+
 class XmlParser {
+
+    companion object {
+
+        private const val EXTENSION = "xml"
+
+        private const val ERROR_MSG_NOT_XML = "The file supplied is not XML!"
+
+        private const val ERROR_MSG_NOT_FOUND = "The file supplied is not found!"
+
+        private const val ERROR_MSG_PARSE_ERR = "Unable to parse xml file"
+
+        fun getDocument(xmlFile: File): Document? {
+
+            if (!xmlFile.exists()) {
+                throw IllegalArgumentException(ERROR_MSG_NOT_FOUND)
+            }
+
+            if (xmlFile.extension != EXTENSION) {
+                throw IllegalArgumentException(ERROR_MSG_NOT_XML)
+            }
+
+            val dbFactory = DocumentBuilderFactory.newInstance()
+            val dBuilder = dbFactory.newDocumentBuilder()
+
+            return dBuilder.parse(xmlFile) ?: throw IllegalAccessError(ERROR_MSG_PARSE_ERR)
+        }
+    }
 }
--- src/main/java/engine/feature/tilemap/TileMap.kt
+++ src/main/java/engine/feature/tilemap/TileMap.kt
@@ -1,12 +1,11 @@
 package engine.feature.tilemap
 
 import com.jogamp.opengl.GL4
+import engine.core.util.xml.XmlParser
 import engine.feature.primitives.Rectangle
 import engine.feature.primitives.Rectangle.RECTANGLE_BUFFER
 import engine.feature.shader.Shader
 import java.io.File
-import java.lang.IllegalArgumentException
-import javax.xml.parsers.DocumentBuilderFactory
 
 class Tile(private val width: Float,
            private val height: Float,
@@ -31,23 +30,11 @@ class TileMap(private val tiles: ArrayList<Tile>) {
 
     companion object {
 
-        private const val ERROR_MSG_NOT_XML = "The file supplied is not XML!"
-
-        private const val ERROR_MSG_NOT_FOUND = "The file supplied is not found!"
-
         fun createTileMap(xmlFile: File): TileMap {
 
-            if (!xmlFile.exists()) {
-                throw IllegalArgumentException(ERROR_MSG_NOT_FOUND)
-            }
+            val document = XmlParser.getDocument(xmlFile)
 
-            if (xmlFile.extension != "xml") {
-                throw IllegalArgumentException(ERROR_MSG_NOT_XML)
-            }
 
-            val dbFactory = DocumentBuilderFactory.newInstance()
-            val dBuilder = dbFactory.newDocumentBuilder()
-            val document = dBuilder.parse(xmlFile)
         }
 
         private fun createTile(xmlTile: String) {
