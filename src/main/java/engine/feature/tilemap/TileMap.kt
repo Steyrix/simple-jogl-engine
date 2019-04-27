8627f5630e989023a8d923c2c803ae4141432c80
Georgy Pevnev <steyrixio@gmail.com>
2019-04-27 17:10:03 +0300

    tileset class impl

--- src/main/java/engine/feature/tilemap/TileMap.kt
+++ src/main/java/engine/feature/tilemap/TileMap.kt
@@ -1,22 +1,24 @@
 package engine.feature.tilemap
 
 import com.jogamp.opengl.GL4
+import com.jogamp.opengl.util.texture.Texture
 import engine.util.xml.XmlParser
 import engine.feature.primitives.Rectangle
 import engine.feature.primitives.Rectangle.RECTANGLE_BUFFER
 import engine.feature.shader.Shader
+import engine.feature.texture.TextureLoader
 import java.io.File
 
 class Tile(private val width: Float,
            private val height: Float,
-           private val texturePath: String,
+           private val texture: Texture,
            private val posX: Float,
            private val posY: Float) {
 
     private fun toOpenGlObject(gl: GL4): Rectangle {
 
         val out = Rectangle(gl, width, height, 0)
-        out.initRenderData(arrayOf(texturePath), false, RECTANGLE_BUFFER)
+        out.initRenderData(texture, RECTANGLE_BUFFER)
 
         return out
     }
@@ -26,23 +28,58 @@ class Tile(private val width: Float,
     }
 }
 
-class TileMap(private val tiles: ArrayList<Tile>) {
+class TileMap(private val tiles: ArrayList<Tile>, private val tileSet: TileSet) {
 
     companion object {
 
-        fun createTileMap(xmlFile: File): TileMap {
+//        fun createTileMap(xmlFile: File): TileMap {
+//
+//            val document = XmlParser.getDocument(xmlFile)
+//            return TileMap(ArrayList())
+//        }
 
-            val document = XmlParser.getDocument(xmlFile)
-
-            return TileMap(ArrayList())
-        }
-
-        private fun createTile(xmlTile: String) {
-
-        }
+//        private fun createTile(xmlTile: String) {
+//
+//        }
     }
 
     fun draw(gl: GL4, shader: Shader) {
         tiles.forEach { it.draw(gl, shader) }
+    }
+}
+
+class TileSet(private val tiles: ArrayList<Tile>,
+              private val tileWidth: Int,
+              private val tileHeight: Int,
+              private val tileCount: Int,
+              private val columnCount: Int,
+              private val texture: Texture) {
+
+    companion object {
+        private const val TILE_SET = "tileset"
+        private const val TILE_WIDTH = "width"
+        private const val TILE_HEIGHT = "height"
+        private const val TILE_COUNT = "tilecount"
+        private const val COLUMN_COUNT = "columns"
+        private const val IMAGE = "image"
+        private const val SOURCE = "source"
+
+        fun createTileSet(xmlFile: File): TileSet {
+            val document = XmlParser.getDocument(xmlFile)!!
+
+            val tileSetNode = document.getElementsByTagName(TILE_SET)
+            val tileSetAttribs = tileSetNode.item(0).attributes
+
+            val tileWidth = tileSetAttribs.getNamedItem(TILE_WIDTH).nodeValue
+            val tileHeight = tileSetAttribs.getNamedItem(TILE_HEIGHT).nodeValue
+            val tileCount = tileSetAttribs.getNamedItem(TILE_COUNT).nodeValue
+            val columnCount = tileSetAttribs.getNamedItem(COLUMN_COUNT).nodeValue
+
+            val imageNode = document.getElementsByTagName(IMAGE)
+            val sourcePath = imageNode.item(0).attributes.getNamedItem(SOURCE).nodeValue
+            val texture = TextureLoader.loadTexture(sourcePath)
+
+            return TileSet(ArrayList(), tileWidth.toInt(), tileHeight.toInt(), tileCount.toInt(), columnCount.toInt(), texture)
+        }
     }
 }
