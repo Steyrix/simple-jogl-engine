4cfdc233d30579e1a50007285c1ea1eb8fcc57c8
Georgy Pevnev <steyrixio@gmail.com>
2020-01-06 12:00:42 +0300

    Implemented robust method for checking properties' values

--- src/main/java/engine/feature/tiled/Tile.kt
+++ src/main/java/engine/feature/tiled/Tile.kt
@@ -1,6 +1,6 @@
 package engine.feature.tiled
 
-//Tile is relative to its tileset
+// Tile is relative to its tileset
 internal class Tile(private val tileUV: FloatArray) {
     val arrayUV
         get() = tileUV
--- src/main/java/engine/feature/tiled/TileMap.kt
+++ src/main/java/engine/feature/tiled/TileMap.kt
@@ -2,12 +2,11 @@ package engine.feature.tiled
 
 import com.jogamp.opengl.GL4
 import engine.feature.shader.Shader
-import engine.feature.tiled.property.BooleanProperty
 import engine.feature.tiled.property.LayerProperty
 import java.io.File
 import kotlin.math.roundToInt
 
-//width and height are measured in tiles
+// width and height are measured in tiles
 class TileMap internal constructor(private val tileLayers: ArrayList<TileLayer>) {
 
     private var currentWidth = 0f
@@ -21,8 +20,8 @@ class TileMap internal constructor(private val tileLayers: ArrayList<TileLayer>)
         tileLayers.forEach { it.draw(gl, xSize, ySize, shader) }
     }
 
-    fun getTileNumberInLayer(posX: Float, posY: Float, layerNumber: Int = 0): Int {
-        val layer = tileLayers[layerNumber]
+    fun getTileIndexInLayerData(posX: Float, posY: Float, layerIndex: Int = 0): Int {
+        val layer = tileLayers[layerIndex]
         val widthInTiles = layer.width
 
         val currentTileWidth = currentWidth / layer.width
@@ -33,19 +32,45 @@ class TileMap internal constructor(private val tileLayers: ArrayList<TileLayer>)
         return yTileNumber * widthInTiles + xTileNumber
     }
 
-    // TODO: add robustness
-    fun getTileProperty(tileNumber: Int, layerNumber: Int = 0, propertyName: String): LayerProperty<out Any> {
-        val layer = tileLayers[layerNumber]
+    fun getPropertyValueForTile(tileIndex: Int, layerIndex: Int = 0, propertyName: String): Any? {
+        if (!arePropertiesActiveForTile(tileIndex, layerIndex)) return null
 
-        return if (tileNumber != 0) {
-            layer.properties.find { it.getName() == propertyName } ?: throw Exception("Property with name $propertyName does not exist")
-        } else {
-            throw Exception("Zero tile value property obtaining ? (Exception to be reworked)")
-        }
+        val property = getLayerProperty(layerIndex, propertyName)
+        return property.value
+    }
+
+    fun getLayerProperty(layerIndex: Int = 0, propertyName: String): LayerProperty<out Any> {
+        val layer = getLayer(layerIndex)
+        return layer.properties.find { it.getName() == propertyName } ?: throw Exception("Property with name $propertyName does not exist")
+    }
+
+    fun arePropertiesActiveForTile(tileIndex: Int, layerIndex: Int = 0): Boolean {
+        val layer = getLayer(layerIndex)
+        return getTile(tileIndex, layer) != 0
     }
 
     override fun toString(): String {
         return "TileMap. Layers count: " + tileLayers.size
+    }
+
+    private fun getLayer(layerIndex: Int): TileLayer {
+        checkLayerExistence(layerIndex)
+        return tileLayers[layerIndex]
+    }
+
+    private fun getTile(tileIndex: Int, layer: TileLayer): Int {
+        checkTileExistence(tileIndex, layer)
+        return layer.tileData[tileIndex]
+    }
+
+    private fun checkLayerExistence(layerIndex: Int) {
+        val incorrectLayerIndex = layerIndex >= tileLayers.size || layerIndex < 0
+        if (incorrectLayerIndex) throw Exception("There is no layer with index $layerIndex")
+    }
+
+    private fun checkTileExistence(tileIndex: Int, layer: TileLayer) {
+        val incorrectTileIndex = tileIndex >= layer.tileData.size || tileIndex < 0
+        if (incorrectTileIndex) throw Exception("There is no tile with number $tileIndex in the layer")
     }
 
     private fun getTiledPosition(tileSize: Float, pos: Float): Int {
