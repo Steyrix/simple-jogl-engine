4f3c2a727aff8cf466e3d33397d24d0fdfee92d0
Georgy Pevnev <steyrixio@gmail.com>
2021-01-03 00:14:20 +0300

    Introduce presets for Labyrinth demo

---
+++ src/main/java/demo/labrynth/LabyrinthPresets.kt
@@ -0,0 +1,19 @@
+package demo.labrynth
+
+import engine.feature.animation.BasicAnimation
+
+data class LabyrinthPresets(
+        val characterPresets: CharacterPresets = CharacterPresets()
+)
+
+data class CharacterPresets(
+        val animation: AnimationsPresets = AnimationsPresets()
+)
+
+data class AnimationsPresets(
+        val animations: MutableList<BasicAnimation> = mutableListOf(
+                BasicAnimation("WALK", 1, 0, 6, 1, 100f),
+                BasicAnimation("JUMP", 2, 0, 3, 1, 200f),
+                BasicAnimation("IDLE", 3, 0, 1, 1, 100f)
+        )
+)
