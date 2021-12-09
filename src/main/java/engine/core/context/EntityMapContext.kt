c0bbd8140c8f65a73d134aed4bf31eebf20d5432
Georgy Pevnev <steyrixio@gmail.com>
2021-12-09 17:33:24 +0300

    Introduce GameContext.kt and its subclass EntityMapContext.kt

---
+++ src/main/java/engine/core/context/EntityMapContext.kt
@@ -0,0 +1,12 @@
+package engine.core.context
+
+import engine.core.entity.Entity
+
+open class EntityMapContext : GameContext {
+
+    protected val entityMap = hashMapOf<String, Entity>()
+    
+    override fun init() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/main/java/engine/core/context/GameContext.kt
@@ -0,0 +1,9 @@
+package engine.core.context
+
+interface GameContext {
+
+    /*
+    This one is for initializing such things as renderers and etc.
+     */
+    fun init()
+}
