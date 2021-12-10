bd642b0634511204246b44cd5c8d697dad45e7a3
Georgy Pevnev <steyrixio@gmail.com>
2021-12-10 15:22:02 +0300

    Leave a comment on topic of dynamically updating points

--- src/main/java/demo/labrynth/character/CharacterBoundingBox.kt
+++ src/main/java/demo/labrynth/character/CharacterBoundingBox.kt
@@ -2,6 +2,9 @@ package demo.labrynth.character
 
 import engine.feature.collision.BoundingBox
 
+/* TODO: think of a way of updating collision points by updating box's position.
+    probably calculating of each point should be a function
+ */
 class CharacterBoundingBox(
         posX: Float,
         posY: Float,
