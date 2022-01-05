92c2300c3efa0168767149761ddee4e40b1625a3
Georgy Pevnev <steyrixio@gmail.com>
2022-01-05 17:43:57 +0300

    Darken bg, remove darking of bounds

--- src/main/resources/shaders/texturedFragmentShader.glsl
+++ src/main/resources/shaders/texturedFragmentShader.glsl
@@ -4,7 +4,7 @@ out vec4 fColor;
 uniform sampler2D textureSample;
 void main(void)
 {
-    fColor = texture(textureSample, fragmentUV).rgba;
+    fColor = texture(textureSample, fragmentUV).rgba * vec4(0.5, 0.5, 0.5, 1.0);
 
     if(fColor.a <= 0){
         discard;
